19d3238b4e2ca1254908a1517f5f2cb90865f8cc
klaushamperl <info@hamperl-design.de>
2025-05-16 19:08:20 +0200

    chore: add tailwind config, finish inital project setup

--- app.vue
+++ app.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1 class="text-6xl font-inter text-primary">
+      Hello World
+    </h1>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/fonts', '@nuxt/icon']
+  modules: [
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@pinia/nuxt'
+  ],
+  css: ['~/assets/css/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  fonts: {
+    families: [
+      {name: 'Inter', provider: 'google'}
+    ]
+  },
 })
