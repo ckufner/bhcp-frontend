35ce212d514946a4788f14be59288bb3d0cd39d6
klaushamperl <info@hamperl-design.de>
2025-05-16 23:35:34 +0200

    refactor: add background for main view

--- app.vue
+++ app.vue
@@ -2,12 +2,14 @@
 import HeaderBar from "~/components/header/header-bar.vue";
 import UserDrawer from "~/components/user-drawer/user-drawer.vue";
 import UserCard from "~/components/user-card/user-card.vue";
+import ApplicationBg from "~/components/other/application-bg.vue";
 </script>
 <template>
-  <div class="font-inter p-8">
+  <div class="font-inter p-8 relative">
     <HeaderBar></HeaderBar>
     <SearchBar />
     <UserDrawer />
     <UserCard :users="['test', 'test']"/>
+    <ApplicationBg />
   </div>
 </template>
---
+++ components/other/application-bg.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div class="w-screen h-screen fixed top-1/3 -z-10">
+  <img src="~/assets/img/hackathon-bg.svg" alt="background image" />
+</div>
+</template>
+
+<style scoped>
+
+</style>
