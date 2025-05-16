32e3e8fe30edd186ec1cf9b41cc3b7dab868d90d
klaushamperl <info@hamperl-design.de>
2025-05-17 00:27:46 +0200

    refactor: add clickAwayListener for drawer

--- components/user-drawer/user-drawer.vue
+++ components/user-drawer/user-drawer.vue
@@ -3,11 +3,17 @@
 import UserEditForm from "~/components/user-drawer/user-edit-form.vue";
 import {userEditStore} from "~/store/user-edit.store";
 
+const drawerElem = ref<HTMLElement | null>(null);
 const editStore = userEditStore()
+
+onClickOutside(drawerElem, () => {
+  editStore.drawerShow = false
+})
 </script>
 
 <template>
 <div
+    ref="drawerElem"
     class="fixed top-0 bg-secondary lg:w-[50vw] w-full h-screen transition-all duration-350 overflow-y-auto"
     :class="[editStore.drawerShow ? 'right-0' : 'right-[-100vw]']"
 >
